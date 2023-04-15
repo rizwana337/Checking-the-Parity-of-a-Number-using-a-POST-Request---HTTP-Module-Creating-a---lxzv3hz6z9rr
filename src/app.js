a5a183ed62474eb981daa553cbb0f879387cf53e
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-15 14:02:40 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,16 +3,24 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   if (req.method === 'POST') {
     const chunks = [];
-
+    console.log(req.body);
     req.on('data', chunk => {
+      console.log(chunk);
       const buf = Buffer.from(chunk);
       const str = buf.toString();
+      console.log(str);
       chunks.push(str);
       const obj = JSON.parse(chunks)
-      const value = obj.num1;
+      const value = obj.num;
+      console.log(obj.num)
     
-     // Write the code here to check if the number is odd or even
-
+     if (value % 2 === 0) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`The number ${value} is even`);
+      } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end(`The number ${value} is odd`);
+      }
    });
   }
 
@@ -21,3 +29,27 @@ const server = http.createServer((req, res) => {
 
 
 module.exports = server;
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   if (req.method === 'POST') {
+//     const chunks = [];
+
+//     req.on('data', chunk => {
+//       const buf = Buffer.from(chunk);
+//       const str = buf.toString();
+//       chunks.push(str);
+//       const obj = JSON.parse(chunks)
+//       const value = obj.num1;
+    
+//      // Write the code here to check if the number is odd or even
+
+//    });
+//   }
+
+  
+// });
+
+
+// module.exports = server;
