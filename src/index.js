cca5442c39b73589b70846268caea0f6b18aa64d
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-15 14:13:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server listening on http://localhost:${port}`);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on http://localhost:${process.env.PORT}`);
   })
+
+
+// const app = require('./app');
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+// const port = 3000;
+// app.listen(port, () => {
+//     console.log(`Server listening on http://localhost:${port}`);
+//   })
 
